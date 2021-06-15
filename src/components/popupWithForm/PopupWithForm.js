35ef65ac473f96e833d834640f564b8549a72b1a
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2021-06-15 20:09:10 +0300

    feat-closePopup

--- src/components/popupWithForm/PopupWithForm.js
+++ src/components/popupWithForm/PopupWithForm.js
@@ -9,13 +9,13 @@ import '../../blocks/pop-up/_active/pop-up_active.css';
 
 const PopupWithForm = (props) => {
     return (
-    <div id={props.name} className={props.isOpen ? 'pop-up_active' : 'pop-up'}>
+    <div id={props.name} className={props.isOpen ? 'pop-up pop-up_active' : 'pop-up'}>
     <div className="pop-up__container pop-up__container_background">
-      <button className="pop-up__btn-close" type="button" aria-label="Close"></button>
+      <button className="pop-up__btn-close" type="button" aria-label="Close" onClick={props.onClose}></button>
       <h2 className="pop-up__title">
         {props.title}
       </h2>
-      <form id="edit-form" className="pop-up__form" action="#" name="popup-form" novalidate>
+      <form id="edit-form" className="pop-up__form" action="#" name={props.name} noValidate>
         {props.children}
         <button className="pop-up__btn-save" type="submit">
           {props.textBtn}
